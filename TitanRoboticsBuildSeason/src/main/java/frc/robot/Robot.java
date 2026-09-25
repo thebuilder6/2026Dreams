fd3fb1e9c90f3fc8d8c94bd345368201fd70adfa
InfiniteQuery <43260133+thebuilder6@users.noreply.github.com>
2026-09-25 15:31:50 -0600

    fix(lifecycle): call teleop.init() on teleopInit to reset input limiters and states

--- TitanRoboticsBuildSeason/src/main/java/frc/robot/Robot.java
+++ TitanRoboticsBuildSeason/src/main/java/frc/robot/Robot.java
@@ -225,6 +225,7 @@ public class Robot extends LoggedRobot {
   /** This function is called once when teleop is enabled. */
   @Override
   public void teleopInit() {
+    teleop.init();
   }
 
   /** This function is called periodically during operator control. */
